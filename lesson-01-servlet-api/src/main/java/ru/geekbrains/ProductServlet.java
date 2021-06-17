6f98af87e40ea953e298c2b376db371737cf7a7f
Dmitry.Moiseev <4553060@gmail.com>
2021-06-18 01:21:18 +0300

    lesson-1

---
+++ lesson-01-servlet-api/src/main/java/ru/geekbrains/ProductServlet.java
@@ -0,0 +1,42 @@
+package ru.geekbrains;
+
+import ru.geekbrains.persist.Product;
+import ru.geekbrains.persist.ProductRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(urlPatterns = "/product")
+public class ProductServlet extends HttpServlet {
+
+    private ProductRepository productRepository;
+
+    @Override
+    public void init() throws ServletException {
+        productRepository = (ProductRepository) getServletContext().getAttribute("productRepository");
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Product> products = productRepository.findAll();
+
+        resp.getWriter().println("<table cellspacing=\"2\" border=\"1\" cellpadding=\"5\" width=\"600\">");
+        resp.getWriter().println("<tr>");
+        for (Product product : products) {
+            resp.getWriter().println("<td>");
+            resp.getWriter().println(product.getName());
+            resp.getWriter().println("</td>");
+        }
+        resp.getWriter().println("</tr>");
+        resp.getWriter().println("</table>");
+
+
+
+    }
+}
