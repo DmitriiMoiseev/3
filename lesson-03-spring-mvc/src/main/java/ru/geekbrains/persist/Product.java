8a07d1643d12376c76f1a1bb7ff929bd1a39d504
Dmitry.Moiseev <4553060@gmail.com>
2021-06-28 23:19:38 +0300

    home work 3

---
+++ lesson-03-spring-mvc/src/main/java/ru/geekbrains/persist/Product.java
@@ -0,0 +1,31 @@
+package ru.geekbrains.persist;
+
+public class Product {
+
+    private Long id;
+
+    private String productname;
+
+    public Product() {
+    }
+
+    public Product(String productname) {
+        this.productname = productname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+}
